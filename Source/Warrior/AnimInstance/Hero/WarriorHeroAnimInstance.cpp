4a9c39316c66521e8973cd314bd8ecfe6e520601
jeyoung <dkfmwpsxm@naver.com>
2026-02-17 23:08:34 +0900

    Feat: Hero AnimInstance에 Idle 대기 시 Relax 상태 전환 로직 추가 - Random Sequence Anim 학습

--- Source/Warrior/AnimInstance/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Warrior/AnimInstance/Hero/WarriorHeroAnimInstance.cpp
@@ -3,3 +3,30 @@
 
 #include "AnimInstance/Hero/WarriorHeroAnimInstance.h"
 
+#include "Character/WarriorHeroCharacter.h"
+
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<AWarriorHeroCharacter>(OwningCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	
+	if (bHasAcceleration)
+	{
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElpasedTime += DeltaSeconds;
+
+		bShouldEnterRelaxState = (IdleElpasedTime >= EnterRelaxtStateThreshold);
+	}
+}
--- Source/Warrior/AnimInstance/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior/AnimInstance/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstance/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -13,5 +14,20 @@ UCLASS()
 class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|Refrences")
+	TObjectPtr<AWarriorHeroCharacter> OwningHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxtStateThreshold = 5.f;
+
+	float IdleElpasedTime;
 };

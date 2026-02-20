a529010b0f50ad17a14bdd2c3130b73da3999025
jeyoung <dkfmwpsxm@naver.com>
2026-02-20 22:42:24 +0900

    Feat: 31 Ability 입력 액션 바인딩 시스템 추가 핵심 c++ : UWarriorInputComponent::BindNativeInputAction Asset: DA_InputConfig

--- Source/Warrior/SharedGameplayTags.cpp
+++ Source/Warrior/SharedGameplayTags.cpp
@@ -2,8 +2,10 @@
 
 namespace WarriorGameplayTags
 {
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
-	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_EquipAxe,"InputTag.EquipAxe");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequipAxe,"InputTag.UnequipAxe");
 
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe,"Player.Weapon.Axe");
 }

0887cb31c629b6ffcdf960d4aa7a6fd0602538a2
jeyoung <dkfmwpsxm@naver.com>
2026-02-22 15:04:51 +0900

    Feat: 35 Anim Montage Notify로 GA 이벤트 수신 - 1번키로 도끼 무장

--- Source/Warrior/SharedGameplayTags.cpp
+++ Source/Warrior/SharedGameplayTags.cpp
@@ -8,4 +8,7 @@ namespace WarriorGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequipAxe,"InputTag.UnequipAxe");
 
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe,"Player.Weapon.Axe");
+	
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Equip_Axe,"Player.Event.Equip.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Unequip_Axe,"Player.Event.Unequip.Axe");
 }
